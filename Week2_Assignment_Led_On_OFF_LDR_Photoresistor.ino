681de3e23df6f247d7e4c44889568b9753037d66
Md. Eftekharul Islam <43173984+Ashique41@users.noreply.github.com>
2018-09-11 20:53:58 +0600

    Add files via upload

---
+++ Week2_Assignment_Led_On_OFF_LDR_Photoresistor.ino
@@ -0,0 +1,25 @@
+//LED turns on when room is dark
+
+#define LED 9
+int sensorValue=0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  sensorValue = analogRead(A0);
+  Serial.println("Light Intensity : ");
+  Serial.println(sensorValue);
+ 
+  delay(1000);   
+  if (sensorValue > 500) { 
+    digitalWrite(LED, HIGH);
+    Serial.println("Light Status : ON ");   
+  }
+ 	 else  {
+    digitalWrite(LED, LOW);
+    Serial.println("Light Status : OFF ");
+  }
+}
