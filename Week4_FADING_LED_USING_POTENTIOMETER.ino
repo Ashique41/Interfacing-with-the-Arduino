59147fc7f30d1456fd8f18a1d677d295ce198e74
Md. Eftekharul Islam <43173984+Ashique41@users.noreply.github.com>
2018-09-12 22:33:00 +0600

    Add files via upload

---
+++ Week4_FADING_LED_USING_POTENTIOMETER.ino
@@ -0,0 +1,26 @@
+#define LED 10
+#define POT A0
+
+int PotValue = 0;
+int LedValue = 0;
+
+void setup() {
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  PotValue = analogRead(POT);
+  
+  LedValue = map(PotValue, 0, 1023, 0, 255);
+  
+  analogWrite(LED, LedValue);
+  
+  Serial.print("Potentiometer = ");
+  Serial.print(PotValue);
+  Serial.print("\t output = ");
+  Serial.println(LedValue);
+  delay(200);
+  
+}
